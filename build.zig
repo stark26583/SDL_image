78e30d6b29fc372427c4abe0a68bba7f41b1e6e0
Jay Petacat <jay@jayschwa.net>
2025-01-03 13:26:04 -0700

    Replace deprecated `defineCMacro` for Zig 0.14.0-dev compat

--- build.zig
+++ build.zig
@@ -19,43 +19,42 @@ pub fn build(b: *std.Build) void {
     });
     const sdl_lib = sdl_dep.artifact("SDL2");
     lib.linkLibrary(sdl_lib);
-    if (sdl_lib.installed_headers_include_tree) |tree|
-        lib.addIncludePath(tree.getDirectory().path(b, "SDL2"));
+    lib.addIncludePath(sdl_lib.getEmittedIncludeTree().path(b, "SDL2"));
 
     // Use stb_image for loading JPEG and PNG files. Native alternatives such as
     // Windows Imaging Component and Apple's Image I/O framework are not yet
     // supported by this build script.
-    lib.defineCMacro("USE_STBIMAGE", null);
+    lib.root_module.addCMacro("USE_STBIMAGE", "");
 
     // The following are options for supported file formats. AVIF, JXL, TIFF,
     // and WebP are not yet supported by this build script, as they require
     // additional dependencies.
     if (b.option(bool, "enable-bmp", "Support loading BMP images") orelse true)
-        lib.defineCMacro("LOAD_BMP", null);
+        lib.root_module.addCMacro("LOAD_BMP", "");
     if (b.option(bool, "enable-gif", "Support loading GIF images") orelse true)
-        lib.defineCMacro("LOAD_GIF", null);
+        lib.root_module.addCMacro("LOAD_GIF", "");
     if (b.option(bool, "enable-jpg", "Support loading JPEG images") orelse true)
-        lib.defineCMacro("LOAD_JPG", null);
+        lib.root_module.addCMacro("LOAD_JPG", "");
     if (b.option(bool, "enable-lbm", "Support loading LBM images") orelse true)
-        lib.defineCMacro("LOAD_LBM", null);
+        lib.root_module.addCMacro("LOAD_LBM", "");
     if (b.option(bool, "enable-pcx", "Support loading PCX images") orelse true)
-        lib.defineCMacro("LOAD_PCX", null);
+        lib.root_module.addCMacro("LOAD_PCX", "");
     if (b.option(bool, "enable-png", "Support loading PNG images") orelse true)
-        lib.defineCMacro("LOAD_PNG", null);
+        lib.root_module.addCMacro("LOAD_PNG", "");
     if (b.option(bool, "enable-pnm", "Support loading PNM images") orelse true)
-        lib.defineCMacro("LOAD_PNM", null);
+        lib.root_module.addCMacro("LOAD_PNM", "");
     if (b.option(bool, "enable-qoi", "Support loading QOI images") orelse true)
-        lib.defineCMacro("LOAD_QOI", null);
+        lib.root_module.addCMacro("LOAD_QOI", "");
     if (b.option(bool, "enable-svg", "Support loading SVG images") orelse true)
-        lib.defineCMacro("LOAD_SVG", null);
+        lib.root_module.addCMacro("LOAD_SVG", "");
     if (b.option(bool, "enable-tga", "Support loading TGA images") orelse true)
-        lib.defineCMacro("LOAD_TGA", null);
+        lib.root_module.addCMacro("LOAD_TGA", "");
     if (b.option(bool, "enable-xcf", "Support loading XCF images") orelse true)
-        lib.defineCMacro("LOAD_XCF", null);
+        lib.root_module.addCMacro("LOAD_XCF", "");
     if (b.option(bool, "enable-xpm", "Support loading XPM images") orelse true)
-        lib.defineCMacro("LOAD_XPM", null);
+        lib.root_module.addCMacro("LOAD_XPM", "");
     if (b.option(bool, "enable-xv", "Support loading XV images") orelse true)
-        lib.defineCMacro("LOAD_XV", null);
+        lib.root_module.addCMacro("LOAD_XV", "");
 
     lib.addIncludePath(upstream.path("include"));
     lib.addIncludePath(upstream.path("src"));
